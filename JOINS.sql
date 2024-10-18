a0b385a9933610260ec4d4b1209ed147aecb6c72
Aakash510 <aakashchourasiya81@gmail.com>
2024-10-18 21:58:08 +0530

    good first commit

---
+++ JOINS.sql
@@ -0,0 +1,90 @@
+USE dx_pvt;
+
+CREATE TABLE employe (
+			emp_id CHAR(5) NOT NULL,
+            emp_nane VARCHAR(20) NOT NULL,
+            salary INT NOT NULL,
+            dept_id CHAR(5) NOT NULL,
+            manager_id CHAR(5) NOT NULL
+);
+ALTER TABLE employe
+RENAME COLUMN emp_nane TO emp_name;
+
+INSERT INTO employe (emp_id,emp_name,salary,dept_id,manager_id)
+VALUES
+('E1','jay',  20000, 'D1', 'M1'),
+('E2','ram',  25000, 'D2', 'M2'),
+('E3','joy',  30000, 'D3', 'M3'),
+('E4','shyam',15000, 'D4', 'M4'),
+('E5','vyom', 45000, 'D5', 'M5');
+
+CREATE TABLE department (
+			dep_id CHAR(5) NOT NULL,
+            dep_name VARCHAR(20) NOT NULL
+);
+ALTER TABLE department
+RENAME COLUMN dep_nane TO dep_name;
+select * from department;
+INSERT INTO department (dep_id,dep_name)
+VALUES
+('D1', 'IT'),
+('D2', 'ME'),
+('D3', 'CE'),
+('D4', 'EE'),
+('D5', 'CSE');
+SET SQL_SAFE_UPDATES = 0;   -- SAFE MODE OFF
+DELETE FROM department
+WHERE dep_id = 'D5';
+INSERT INTO department (dep_id,dep_name)
+VALUES
+('D5', 'CSE');
+CREATE TABLE manager (
+			man_id CHAR(5) NOT NULL,
+            man_name VARCHAR(20) NOT NULL,
+            dep_id CHAR(5) NOT NULL
+);
+
+INSERT INTO manager (man_id,man_name,dep_id)
+VALUES
+('M1','rohan',  'D1'),
+('M2','aakash', 'D2'),
+('M3','radhika','D3'),
+('M4','satyam', 'D4');
+
+CREATE TABLE project (
+			project_id CHAR(5) NOT NULL,
+            project_name VARCHAR(20) NOT NULL,
+            team_mem_id CHAR(5) NOT NULL
+);
+
+INSERT INTO project (project_id,project_name,team_mem_id)
+VALUES
+('P1','ELT',  'T1'),
+('P2','DATA_W', 'T2'),
+('P3','PIPELINE','T3'),
+('P4','COLLECTION', 'T4'),
+('P5','UPLOADING', 'T5');
+
+-- INNER JOIN/JOINS (FETCH COMMON dept_id VALUES)
+SELECT e.emp_name , d.dep_name
+FROM employe as e
+INNER JOIN department as d
+ON e.dept_id = d.dep_id;
+
+-- LEFT JOIN (fetch All employe name & their department name)
+SELECT e.emp_name , d.dep_name 	
+FROM employe AS e
+LEFT JOIN department AS d
+ON e.dept_id = d.dep_id;
+
+-- RIGHT JOIN (fetch employe name & ALL department name)
+SELECT emp_id, dep_name 	
+FROM employe AS e
+RIGHT JOIN department AS d
+ON e.dept_id = d.dep_id;
+
+-- Q) fetch the detail of all the employe their manager their department & their project they work on?
+
+SELECT e.emp_name
+FROM employe as e 
+
---
+++ N1.sql
@@ -0,0 +1,99 @@
+CREATE DATABASE dx_pvt;
+USE dx_pvt; 
+
+CREATE TABLE orders (
+            order_id INT NOT NULL,
+            order_no int NOT NULL,
+            quantity INT NOT NULL,
+            product_price INT NOT NULL,
+            total_cost INT NOT NULL,
+            order_date int NOT NULL,
+            status varchar(20),
+            deal_size varchar(20)
+);
+ALTER TABLE orders
+ADD constraint pk_order_id PRIMARY KEY (order_id);
+
+CREATE TABLE product (
+            product_code varchar(20) NOT NULL,
+            product_name varchar(20) NOT NULL
+);
+ALTER TABLE product
+ADD constraint pk_product_code PRIMARY KEY (product_code);
+
+
+CREATE TABLE customer (
+            customer_id INT NOT NULL,
+            customer_first_name varchar(20) NOT NULL,
+            customer_last_name varchar(20) NOT NULL,
+            phone_no INT NOT NULL,
+            address INT NOT NULL,
+            city  varchar(20),
+            state varchar(20),
+            postal_code INT NOT NULL,
+			country varchar(20)
+);
+ALTER TABLE customer
+ADD CONSTRAINT pk_customer_id PRIMARY KEY (customer_id);
+
+ALTER TABLE customer
+MODIFY customer_id CHAR(5) NOT NULL;
+ALTER TABLE Customer
+MODIFY Phone_no BIGINT NOT NULL;
+ALTER TABLE customer
+MODIFY city VARCHAR(20) NOT NULL;
+
+
+CREATE TABLE relationships (
+            order_id INT NOT NULL,
+            product_code varchar(20) NOT NULL,
+            customer_id INT NOT NULL
+);
+ALTER TABLE relationships
+MODIFY COLUMN customer_id CHAR(5);
+
+ALTER TABLE relationships
+ADD CONSTRAINT pk_order_id PRIMARY KEY (order_id);
+
+
+-- BUILDING RELATIONSHIP USING FOREIGH KEYS & PRIMARY KEYS
+
+ALTER TABLE relationships
+ADD CONSTRAINT fk_order_id FOREIGN KEY (order_id) REFERENCES orders (order_id) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE relationships
+ADD CONSTRAINT fk_product_code FOREIGN KEY (product_code) REFERENCES product (product_code) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE relationships
+ADD CONSTRAINT fk_customer_id FOREIGN KEY (customer_id) REFERENCES customer (customer_id) ON DELETE CASCADE ON UPDATE CASCADE;
+
+INSERT INTO orders (order_id,order_no,quantity,product_price,total_cost, order_date ,status,deal_size)
+VALUES
+(1,	101,	2,	200,	400,	'2024-10-01',	"shipped",	"small"),
+(2,	102,	2,	250	,	500,	'2024-10-02',	"cancel",	"medium"),
+(3,	103,	3,	600,	1800,	'2024-10-03',	"shipped",	"big"),
+(4,	103,	3,	600,	1800,	'2024-10-04',	"shipped",	"big"),
+(5,	104,	5,	250,    1250,	'2024-10-05',	"shipped",	"big"),
+(6,	105,	4,	80,		320,	'2024-10-06',	"shipped",	"medium"),
+(7,	106,	2,	100,	200,	'2024-10-07',	"on hold",	"small");
+
+INSERT INTO product (product_code,Product_name)
+values 
+("PRD_101",	"shirt"),
+("PRD_102", "jeans"),
+("PRD_103",	"kurta"),
+("PRD_104",	"Towser"),
+("PRD_105",	"hanky"),
+("PRD_106",	"t-shirt");
+
+INSERT INTO customer (customer_id, Customer_first_name, Customer_last_name, Phone_no, City, state, postal_code, country) VALUES
+(1, 'jay', 'sahu', 9798653245, 'indore', 'mp', 452020, 'India'),
+(2, 'raj', 'panwar', 9844444643, 'bhopal', 'mp', 458520, 'India'),
+(3, 'vyom', 'bisani', 9899746464, 'pipariya', 'mp', 461775, 'India'),
+(4, 'ram', 'rawat', 9594897687, 'dewas', 'mp', 436521, 'India'),
+(5, 'shyam', 'patil', 9798653245, 'ujjain', 'mp', 695262, 'India'),
+(6, 'goli', 'sahu', 9798653245, 'indore', 'mp', 452021, 'India');
+
+		
+SELECT * FROM orders;
+SELECT * FROM product;
+SELECT * FROM customer;
+SELECT * FROM relationships;
