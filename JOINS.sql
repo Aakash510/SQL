65cbf6d9b6147cb4b3a81952f2878dae519e003a
Aakash510 <aakashchourasiya81@gmail.com>
2024-10-20 17:04:04 +0530

    Updates joins statements

--- JOINS.sql
+++ JOINS.sql
@@ -80,6 +80,25 @@ END
 WHERE project_id IN ('P1', 'P2', 'P3', 'P4', 'P5');
 SELECT * FROM project;
 
+CREATE TABLE family (
+			member_id CHAR(5) NOT NULL,
+            member_name VARCHAR(20) NOT NULL,
+            parent_id CHAR(5) NOT NULL
+);
+
+INSERT INTO family (member_id,member_name,parent_id)
+VALUES
+('F1','ARPIT', ''),
+('F2','AAKRSH', 'F8'),
+('F3','MAYANK','F2'),
+('F4','JAYANT', 'F1'),
+('F5','RADHIKA', ''),
+('F6','RAJESH', ''),
+('F7','RHITIK', 'F4'),
+('F8','SHARAD', 'F6');
+SELECT * From family;
+
+
 -- INNER JOIN/JOINS (FETCH COMMON dept_id VALUES)
 SELECT e.emp_name , d.dept_name
 FROM employe as e
@@ -103,6 +122,65 @@ SELECT e.emp_name, d.dept_name, m.mang_name, p.project_name
 FROM employe as e
 JOIN department as d on e.dept_id = d.dept_id
 left join manager as m 	on m.dept_id = e.dept_id
-join project AS p on p.team_mem_id = e.emp_id  
+join project AS p on p.team_mem_id = e.emp_id;  
 
 -- full join ( inner join + all remaining record from left & right table both)
+--  In mysql full join systax doesn't exist,
+--  we use union keyword to with left & right join together to make full join
+SELECT e.emp_name, e.salary, m.dept_id,mang_name
+FROM employe as e
+left join manager as m
+ON e.manager_id = m.mang_id
+UNION
+SELECT e.emp_name, e.salary,m.dept_id, m.mang_name
+FROM employe as e
+right join manager as m
+ON e.manager_id = m.mang_id;
+
+-- cross join it will return cartesian product
+-- no need to give on condition
+-- it will fetch all correspondes column record from left to right table one by one
+-- [it is used in case when onother table dosn't contain matching column
+-- or tabels unable to join but still need to join]
+SELECT emp_name, dept_name
+FROM employe as e
+cross join department as d;
+
+-- Natural join, joins tables automatically by SQL after find  matched column_name in both tables
+-- no need to give ON matching condition
+-- if unable to find matched condition it will join tables as CROSS JOIN and give cartician product
+SELECT emp_name,salary
+FROM employe
+NATURAL JOIN department;
+
+ALTER TABLE department
+RENAME COLUMN dept_id to id;
+SELECT * FROM department;    -- After this modification natural join work as a cross join & give cartesian products.
+
+ALTER TABLE department      -- Undo modification
+RENAME COLUMN id to dept_id;
+SELECT * FROM department;
+
+-- SELF join, is used when have to fetch detail form same table
+-- have to fetch details by two column condition from same table
+-- can aslo use as left,right join.
+SELECT * FROM family;
+
+SELECT c.member_name as Child_Name, 
+	   p.parent_id as Parent_name
+FROM family as c
+JOIN family as p
+on c.member_id = p.parent_id;
+
+SELECT c.member_name as Child_Name,   -- WITH LEFT JOIN 
+	   p.parent_id as Parent_name
+FROM family as c
+LEFT JOIN family as p
+on c.member_id = p.parent_id;
+		
+SELECT * FROM employe;
+SELECT * FROM department;
+SELECT * FROM manager;
+SELECT * FROM project;
+SELECT * FROM family;
+
